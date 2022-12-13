9e4a5cbe1c5b4cd11272537f4dd6ec4dd72b2857
Filippo <filippo.vallarino@gmail.com>
2022-12-13 15:50:35 +0100

    Creata struttura html di base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MailViewComponent } from './mail-view/mail-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FolderListComponent } from './folder-list/folder-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageViewerComponent } from './message-viewer/message-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MailViewComponent,
+    FolderListComponent,
+    MessageListComponent,
+    MessageViewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    /* Angular Material */
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/message-viewer/message-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-viewer',
+  templateUrl: './message-viewer.component.html',
+  styleUrls: ['./message-viewer.component.css']
+})
+export class MessageViewerComponent {
+
+}
