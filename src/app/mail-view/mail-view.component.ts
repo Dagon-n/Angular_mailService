7d037e3b7506fd9cb75be808a1653c2f0be20693
Filippo <filippo.vallarino@gmail.com>
2022-12-15 14:35:04 +0100

    Ora message-list prende dati in input

--- src/app/mail-view/mail-view.component.ts
+++ src/app/mail-view/mail-view.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mail-view',
   templateUrl: './mail-view.component.html',
   styleUrls: ['./mail-view.component.css']
 })
-export class MailViewComponent implements OnInit{
+export class MailViewComponent {
 
-  constructor() {}
+  mailList: 
+    { from: string, to: string, subject: string, body: string, id: number }[] = [
+    { 'from': 'mario.rossi@gmail.com', 
+      'to': 'filippo.vallarino@gmail.com',
+      'subject': 'angular part 1',
+      'body': 'a b c d e f g',
+      'id': 1
+    },
+    { 'from': 'andrea.bianchi@gmail.com', 
+      'to': 'filippo.vallarino@gmail.com',
+      'subject': 'angular part 2',
+      'body': 'a b c d e f g h i',
+      'id': 2
+    },
+    { 'from': 'luca.verdi@gmail.com', 
+      'to': 'filippo.vallarino@gmail.com',
+      'subject': 'angular part 3',
+      'body': 'a b c d e f g j i l m n o',
+      'id': 3
+    },
+    { 'from': 'filippo.vallarino@gmail.com', 
+      'to': 'giulio.bruni@gmail.com',
+      'subject': 'risposta angular',
+      'body': '1 2 3 4 5 6 7',
+      'id': 4
+    }
+  ];
 
-  ngOnInit(): void {
-  }
+  mailRicevute = this.mailList.filter( function(mail : {
+  from: string, 
+  to: string, 
+  subject: string, 
+  body: string, 
+  id: number
+  }): boolean {
+    return mail.to == 'filippo.vallarino@gmail.com'
+  });
+
+  mailInviate = this.mailList.filter( function(mail : {
+  from: string, 
+  to: string, 
+  subject: string, 
+  body: string, 
+  id: number
+  }): boolean {
+    return mail.from == 'filippo.vallarino@gmail.com'
+  });
+
+  toShow = 2
 
 }
--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message-list',
@@ -7,54 +7,6 @@ import { Component } from '@angular/core';
 })
 export class MessageListComponent {
 
-  userLoggato = 'filippo.vallarino@gmail.com';
-
-  mailList: { from: string, to: string, subject: string, body: string, id: number }[] = [
-    { 'from': 'mario.rossi@gmail.com', 
-      'to': 'filippo.vallarino@gmail.com',
-      'subject': 'angular part 1',
-      'body': 'a b c d e f g',
-      'id': 1
-    },
-    { 'from': 'andrea.bianchi@gmail.com', 
-      'to': 'filippo.vallarino@gmail.com',
-      'subject': 'angular part 2',
-      'body': 'a b c d e f g h i',
-      'id': 2
-    },
-    { 'from': 'luca.verdi@gmail.com', 
-      'to': 'filippo.vallarino@gmail.com',
-      'subject': 'angular part 3',
-      'body': 'a b c d e f g j i l m n o',
-      'id': 3
-    },
-    { 'from': 'filippo.vallarino@gmail.com', 
-      'to': 'giulio.bruni@gmail.com',
-      'subject': 'risposta angular',
-      'body': '1 2 3 4 5 6 7',
-      'id': 4
-    }
-  ];
-
-  mailRicevute = this.mailList.filter( function(mail : {
-    from: string, 
-    to: string, 
-    subject: string, 
-    body: string, 
-    id: number
-  }): boolean {
-    return mail.to == 'filippo.vallarino@gmail.com'
-  });
-
-
-  mailInviate = this.mailList.filter( function(mail : {
-    from: string, 
-    to: string, 
-    subject: string, 
-    body: string, 
-    id: number
-  }): boolean {
-    return mail.from == 'filippo.vallarino@gmail.com'
-  });
+  @Input() mailList: any;
 
 }
