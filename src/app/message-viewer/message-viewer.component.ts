5980c53adf41ee20a08faa74c5497d6f09b95808
Filippo <filippo.vallarino@gmail.com>
2022-12-23 10:26:10 +0100

    Se nel cestino, non mostra i bottoni

--- src/app/message-viewer/message-viewer.component.ts
+++ src/app/message-viewer/message-viewer.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-message-viewer',
   templateUrl: './message-viewer.component.html',
   styleUrls: ['./message-viewer.component.css']
 })
-export class MessageViewerComponent {
+export class MessageViewerComponent implements OnChanges {
 
   @Input() mailToShow: any /* Da un errore se di tipo 'Object' */
   @Output() datiCancellazione = new EventEmitter<any>
+
+  toShow = true
+  ngOnChanges() {
+    if(this.mailToShow.isDeleted == true) {
+      this.toShow = false
+    }else{
+      this.toShow = true
+    }
+  }
 
   handleCancellaMail(mail: any) {
     this.datiCancellazione.emit(mail)
