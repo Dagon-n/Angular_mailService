ef39795d95aee21f8f9283ddefdc9b96b31175cc
Filippo <filippo.vallarino@gmail.com>
2022-12-23 10:38:49 +0100

    Ricarica folder al cambio click selezionato

--- src/app/mail-view/mail-view.component.ts
+++ src/app/mail-view/mail-view.component.ts
@@ -32,11 +32,11 @@ export class MailViewComponent implements OnInit {
       next: (data) => {
         this.tutteLeMail = data
         this.inviate = data.filter((mail:any) => mail.from == this.utenteLoggato && mail.isDeleted == false)
-        let mailRicevute = data.filter((mail:any) => mail.to == this.utenteLoggato && mail.isDeleted == false)
-        this.ricevute = mailRicevute
+        let ricevute = data.filter((mail:any) => mail.to == this.utenteLoggato && mail.isDeleted == false)
+        this.ricevute = ricevute
         this.cancellate = data.filter((mail:any) => mail.isDeleted == true)
         this.preferiti = data.filter((mail:any) => mail.isFavourite == true)
-        this.firstMailToShow = mailRicevute[0]
+        this.firstMailToShow = ricevute[0]
       },
       error: (err: Error) => console.warn('Errore!', err),
       complete: () => {
@@ -45,36 +45,100 @@ export class MailViewComponent implements OnInit {
       }
 
     })
-
   }
 
+  changeInviate() {
+    this.fetchingData.getMails().subscribe({
+      next: (data) => {
+        let inviate = data.filter((mail:any) => mail.from == this.utenteLoggato && mail.isDeleted == false)
+        this.inviate = inviate
+        this.firstMailToShow = inviate[0]
+      },
+      error: (err: Error) => console.warn('Errore!', err),
+      complete: () => {
+        this.finishedLoading = true
+        console.log('is loading finished? ', this.finishedLoading)
+      }
+    })
+  }
+
+  changeRicevute() {
+    this.fetchingData.getMails().subscribe({
+      next: (data) => {
+        let ricevute = data.filter((mail:any) => mail.to == this.utenteLoggato && mail.isDeleted == false)
+        this.ricevute = ricevute
+        this.firstMailToShow = this.ricevute[0]
+      },
+      error: (err: Error) => console.warn('Errore!', err),
+      complete: () => {
+        this.finishedLoading = true
+        console.log('is loading finished? ', this.finishedLoading)
+      }
+    })
+  }
+
+  changeCancellate() {
+    this.fetchingData.getMails().subscribe({
+      next: (data) => {
+        let cancellate = data.filter((mail:any) => mail.isDeleted == true)
+        this.cancellate = cancellate
+        this.firstMailToShow = cancellate[0]
+      },
+      error: (err: Error) => console.warn('Errore!', err),
+      complete: () => {
+        this.finishedLoading = true
+        console.log('is loading finished? ', this.finishedLoading)
+      }
+    })
+  }
+
+  changePreferiti() {
+    this.fetchingData.getMails().subscribe({
+      next: (data) => {
+        let preferiti = this.preferiti = data.filter((mail:any) => mail.isFavourite == true)
+        this.preferiti = preferiti
+        this.firstMailToShow = this.preferiti[0]
+      },
+      error: (err: Error) => console.warn('Errore!', err),
+      complete: () => {
+        this.finishedLoading = true
+        console.log('is loading finished? ', this.finishedLoading)
+      }
+    })
+  }
+
+  /* Quando viene cambiata cartella */
   onCartellaSelezionata(value: string) {
 
     this.toShow = value
     switch(value) {
       case 'Inviata':
+        this.changeInviate()
         this.firstMailToShow = this.inviate[0]
         break;
       case 'Cestino':
+        this.changeCancellate()
         this.firstMailToShow = this.cancellate[0]
         break;
       case 'Preferiti':
+        this.changePreferiti()
         this.firstMailToShow = this.preferiti[0]
         break;
       default:
+        this.changeRicevute()
         this.firstMailToShow = this.ricevute[0]
     }
 
-    this.fetchingMails()
-
   }
 
+  /* Quando viene selezionata una mail */
   onMailSelezionata(value: any) {
 
     this.firstMailToShow = value
 
   }
 
+  /* Quando viene cancellata una mail */
   onCancellazioneMail(mail: any) {
     this.fetchingMails()
     this.fetchingData.spostaNelCestino(mail)
